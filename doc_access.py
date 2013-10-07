d51a87ec59ec8f5261ad63759911157b0f5d0050
gchapa22 <gerardo.chapa.q@gmail.com>
2013-10-07 14:41:16 -0500

    Update doc_access.py
    Changed to a function so it can be called from run_me.py

--- doc_access.py
+++ doc_access.py
@@ -1,42 +1,49 @@
 import gspread as G
 import getpass
 
-print 'Bienvenido!'
-print 'Para acceder por favor escribe tus datos.'
+def docu():
+	x = 1
 
-x = 1
+	while x == 1:
+		usr = raw_input('gmail:')
 
-while x == 1:
-	usr = raw_input('gmail:')
+		pwd = getpass.getpass('password:')
 
-	pwd = getpass.getpass('password:')
+		try:
+			gc = G.login(usr, pwd)
+			x = 2
+		except:
+			print 'No se pudo acceder a tu cuenta.'
+			wants = raw_input('Deseas intentar de nuevo (si/no)? ')
+			if wants == 'si' or wants == 'Si' or wants == 'SI' or wants == 's':
+				x == 1
+			else:
+				print 'Adios.'
+				exit()
 
-	try:
-		gc = G.login(usr, pwd)
-		x = 2
-	except:
-		print 'No se pudo acceder a tu cuenta.'
-		wants = raw_input('Deseas intentar de nuevo (si/no)? ')
-		if wants == 'si' or wants == 'Si' or wants == 'SI' or wants == 's':
-			x == 1
-		else:
-			print 'Adios.'
-			exit()
-
-arch = raw_input('Nombre, llave o URL del archivo:')
-try:
-	print 'Accediendo al archivo %s...' % arch
-	ss = gc.open(arch)
-except:
+	arch = raw_input('Nombre, llave o URL del archivo:')
 	try:
 		print 'Accediendo al archivo %s...' % arch
-		ss = gc.open_by_key(arch).sheet1
+		ss = gc.open(arch)
 	except:
 		try:
 			print 'Accediendo al archivo %s...' % arch
-			ss = gc.open_by_url(arch).sheet1
+			ss = gc.open_by_key(arch).sheet1
 		except:
-			print 'No se pudo acceder a este archivo.'
-			exit()
+			try:
+				print 'Accediendo al archivo %s...' % arch
+				ss = gc.open_by_url(arch).sheet1
+			except:
+				print 'No se pudo acceder a este archivo.'
+				exit()
 
-print ss.col_values(1)
+	cols = []
+	n = 1
+	col = col = ss.col_values(n)
+
+	while col:
+		n+=1
+		cols.append(col)
+		col = ss.col_values(n)
+	
+	return cols
