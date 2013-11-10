809431928e4e7c7df2558de5d5adf7ee76bc9cb2
gchapa22 <gerardo.chapa.q@gmail.com>
2013-11-10 14:44:39 -0600

    Update run_me.py

--- run_me.py
+++ run_me.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import sys
 from doc_access import session, get_cols_from_doc 
+from acentos import cambia_caracteres_utf
 
 # Mensaje de Bienvenida
 print 'Bienvenido!'
@@ -23,7 +25,7 @@ except:
 try:
 	from gexf import Gexf as gx
 except:
-	print 'Porfavor instala gefx http://pythonhosted.org/pygexf/users.html/'
+	print 'Porfavor instala gexf http://pythonhosted.org/pygexf/users.html/'
 	error_lib = True
 
 # Si error_lib es verdadero entonces 
@@ -38,25 +40,48 @@ print 'Para acceder por favor escribe tus datos.'
 # la cual crea la sesion en Google Docs
 session()
 
+# Se obtiene las celdas del archivo con los nodos
 cols_sna_nodos = get_cols_from_doc('https://docs.google.com/spreadsheet/ccc?key=0Am6c--_scDIGdDZZbmJycXYtZ21WckVUYUFseW5wZXc&usp=drive_web#gid=0')
 
+# Se obtiene las celdas del archivo con los links
 cols_sna_links = get_cols_from_doc('https://docs.google.com/spreadsheet/ccc?key=0Am6c--_scDIGdC1VU01ZNGNCeUR2RHdqMGVxMkdINVE&usp=drive_web#gid=0')
-
-print cols_sna_nodos[1][1]
 
 # Se crea el grafo con gexf
 gexf = gx("Elba Esther Gordillo", "Analisis de la red social politica en Mexico")
 graph = gexf.addGraph("directed","static", "Red Social Politica en Mexico")
 
 n = 1
-# Se crean los nodos con gexf
+e = 0
+# Se crean los nodos con gexf
 for node in cols_sna_nodos[1]:
 	try:
-		graph.addNode(n, node)
-		print node
+		node = node.encode("utf-8")
+		if isinstance(node, unicode):
+			decoded = node.decode("utf-8")
+			decoded = cambia_caracteres_utf(decoded)
+			print node, "es utf"
+		else:
+			decoded = node
+		if decoded not in graph._nodes and decoded != 'Nombre completo del actor político':
+			graph.addNode(n, decoded)
+			# print "Se creo el nodo %s" % node.decode("utf-8")
+			# print n
+			n+=1
+		#print node
 	except:
-		print "Error %s" % node
-	n+=1
+		#err = cambia_caracteres_utf(node.decode("utf-8"))
+		print "Error %s" % sys.exc_info()[0] 
+		print "en el nodo %s" % node
+		e+=1
+print e
+	
+# for link in cols_sna_links:
+# 	try:
+# 		graph.addLink(n, node.decode("utf-8"))
+# 		print node
+# 	except:
+# 		print "Error %s" % node
+# 	n+=1
 
 output_file = open("red_politica_mex.gexf", "w")
 
